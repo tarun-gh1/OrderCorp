916919cb772c5f6fd0007df377a03425ab023379
tarun-gh1 <42410931+tarun-gh1@users.noreply.github.com>
2020-07-15 15:12:41 +0530

    Final commit

--- OrderCorp/Program.cs
+++ OrderCorp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OrderCorpLibrary;
+using OrderCorpLibrary.ExtDependencies;
+using System;
 
 namespace OrderCorp
 {
@@ -6,7 +8,33 @@ namespace OrderCorp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool orderStatus = false;
+            IPacking packing = new PackingSlip();
+            INotify notify = new Notify();
+            IMembership membership = new Membership();
+
+            var order = new OrderCorpLib(packing, notify, membership);
+
+            // Physical Product
+            IProduct product = new Product()
+            {
+                ProductType = (int)ProductTypes.ProdType.Video,
+                ProductName = "Learning to Ski",
+                Purpose = "Activate"   // OR Upgrade
+
+            };
+            orderStatus = order.CreateOrder(product);
+            
+            if (orderStatus == true)
+            {
+                Console.WriteLine("Order created.");
+            }
+            else
+            {
+                Console.WriteLine("Error creating Order. Please check.");
+            }
+            Console.ReadLine();
+       
         }
     }
 }
